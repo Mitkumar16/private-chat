c6b175126229706b0816d185233b5095c62713cc
Mitkumar <147838518+Mitkumar16@users.noreply.github.com>
2025-05-03 13:27:57 +0530

    responsive done

--- dist/script.js
+++ dist/script.js
@@ -3,9 +3,9 @@ const form = document.getElementById('chat-form');
 const input = document.getElementById('message-input');
 const messages = document.getElementById('messages');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
   e.preventDefault();
-  if (input.value.trim()) {
+  if (input.value) {
     const myMsg = document.createElement('div');
     myMsg.textContent = `You: ${input.value}`;
     myMsg.classList.add('my-message');
@@ -16,7 +16,7 @@ form.addEventListener('submit', function(e) {
   }
 });
 
-socket.on('chat message', function(msg) {
+socket.on('chat message', function (msg) {
   const newMsg = document.createElement('div');
   newMsg.textContent = `Friend: ${msg}`;
   newMsg.classList.add('friend-message');
